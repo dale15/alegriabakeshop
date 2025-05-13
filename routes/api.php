a0f86ed32adbb19045094dbe329eddadd6cb5375
Dale <josephdalebertcajida@gmail.com>
2025-05-13 10:44:54 +0800

    added category API

--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,11 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\Api\CategoryController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\ProductController;
 
 Route::apiResource('products', ProductController::class);
+Route::apiResource('category', CategoryController::class);
 
 // Route::get('/user', function (Request $request) {
 //     return $request->user();
