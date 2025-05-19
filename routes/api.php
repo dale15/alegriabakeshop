66ad2d7e0294de9d367ef49715384520bbf3d21c
Dale <josephdalebertcajida@gmail.com>
2025-05-19 10:01:28 +0800

    added discount api

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\CategoryController;
+use App\Http\Controllers\Api\DiscountController;
 use App\Http\Controllers\Api\SalesController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\ProductController;
@@ -8,6 +9,7 @@ use App\Http\Controllers\Api\ProductController;
 Route::apiResource('products', ProductController::class);
 Route::apiResource('category', CategoryController::class);
 Route::apiResource('sales', SalesController::class);
+Route::apiResource('discount', DiscountController::class);
 
 // Route::get('/user', function (Request $request) {
 //     return $request->user();
